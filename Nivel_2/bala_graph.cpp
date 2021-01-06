59ffec7376c2aebb830f7ace603b9e34e637c299
Oscar Gutiérrez <oscar.gutierrezr@udea.edu.co>
2021-01-06 17:48:33 -0500

    Parte gráfica y movimiento de la bala

--- Nivel_2/bala_graph.cpp
+++ Nivel_2/bala_graph.cpp
@@ -1,6 +1,30 @@
 #include "bala_graph.h"
 
-Bala_graph::Bala_graph()
+Bala_graph::Bala_graph(double x, double y, double v, double a)
 {
+    bala = new Bala(x,y,v,a);
 
+    timer = new QTimer;
+    connect(timer, SIGNAL(timeout()), this, SLOT(Actualizar()));
+}
+
+QRectF Bala_graph::boundingRect() const
+{
+    return QRectF(-bala->getR(),-bala->getR(),2*bala->getR(),2*bala->getR());
+}
+
+void Bala_graph::paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget)
+{
+    painter->setBrush(Qt::darkCyan);
+    painter->drawEllipse(boundingRect());
+}
+
+Bala *Bala_graph::getBala() const
+{
+    return bala;
+}
+
+void Bala_graph::Actualizar()
+{
+    bala->ActualizarPosicion();
 }
--- Nivel_2/bala_graph.h
+++ Nivel_2/bala_graph.h
@@ -1,11 +1,25 @@
 #ifndef BALA_GRAPH_H
 #define BALA_GRAPH_H
 
+#include "bala.h"
+#include <QGraphicsItem>
+#include <QPainter>
+#include <QObject>
+#include <QTimer>
 
-class Bala_graph
+class Bala_graph :public QGraphicsItem, public QObject
 {
+    Bala *bala;
+    QTimer *timer;
+
 public:
-    Bala_graph();
+    Bala_graph(double x, double y,double v, double a);
+    QRectF boundingRect() const;
+    void paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget = Q_NULLPTR);
+    Bala *getBala() const;
+
+private slots:
+    void Actualizar();
 };
 
 #endif // BALA_GRAPH_H
