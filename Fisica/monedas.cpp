361041b4c4b43455ab40a90fe939171ef60f7e2e
OscarG356 <oscar.gutierrezr@udea.edu.co>
2021-01-15 23:38:24 -0500

    Sprite para las monedas añadido

--- Fisica/monedas.cpp
+++ Fisica/monedas.cpp
@@ -5,6 +5,37 @@ monedas::monedas(int x, int y)
     posx = x;
     posy = y;
     setPos(posx,posy);
+
+    columnas = 0;
+    timer = new QTimer();
+    pixmap = new QPixmap(":/new/fondo/Sprite_moneda.png");
+    setScale(0.5);
+
+    timer->start(100);
+    connect(timer,&QTimer::timeout,this,&monedas::Actualizar);
+}
+
+QRectF monedas::boundingRect() const
+{
+    //return QRectF(-r,-r,2*r,2*r);
+    return QRectF(-ancho/2,-alto/2,ancho,alto);
+}
+
+void monedas::paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget)
+{
+//    painter->setBrush(Qt::yellow);
+//    painter->drawEllipse(boundingRect());
+
+    painter->drawPixmap(-ancho/2,-alto/2,*pixmap,columnas,0,ancho,alto);
+}
+
+void monedas::Actualizar()
+{
+    columnas+=75;
+    if(columnas>600){
+        columnas=0;
+    }
+    this->update(-ancho/2,-alto/2,ancho,alto);
 }
 
 int monedas::getR() const
@@ -35,16 +66,4 @@ int monedas::getPosy() const
 void monedas::setPosy(int value)
 {
     posy = value;
-}
-
-QRectF monedas::boundingRect() const
-{
-    return QRectF(-r,-r,2*r,2*r);
-}
-
-void monedas::paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget)
-{
-    painter->setBrush(Qt::yellow);
-    painter->drawEllipse(boundingRect());
-
 }
--- Fisica/monedas.h
+++ Fisica/monedas.h
@@ -2,13 +2,19 @@
 #define MONEDAS_H
 
 #include <QGraphicsItem>
+#include <QObject>
 #include <QPainter>
+#include <QPixmap>
+#include <QTimer>
 
-class monedas: public QGraphicsItem
+class monedas: public QObject, public QGraphicsItem
 {
-
     int r=10;
     int posx, posy;
+    int ancho = 75, alto = 70;
+    int columnas;
+    QPixmap *pixmap;
+    QTimer *timer;
 
 public:
     monedas(int x, int y);
@@ -22,6 +28,9 @@ public:
 
     QRectF boundingRect() const;
     void paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget = nullptr);
+
+public slots:
+    void Actualizar();
 
 };
 #endif // MONEDAS_H
